52ff5cd57553a0ff229214758b5e397c74e396e0
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-01-16 14:24:36 +0530

    correction

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,13 @@
-(("recipes"
-  (nil (eval . (and buffer-file-name
-		    (not (eq major-mode 'package-recipe-mode))
-		    (or (require 'package-recipe-mode nil t)
-			(let ((load-path (cons "../package-build" load-path)))
-			  (require 'package-recipe-mode nil t)))
-		    (package-recipe-mode))))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+((nil . ((eval . (set (make-local-variable 'projectile-project-root)
+                      (locate-dominating-file default-directory ".dir-locals.el")))
+         ;; (eval . (set (make-local-variable 'python-shell-interpreter-args)
+         ;;              (format "--project-directory %s/build exec -T --privileged fortiguest ipython --simple-prompt -i"
+         ;;                      (locate-dominating-file default-directory ".dir-locals.el"))))
+         ;; (python-shell-interpreter . "docker-compose")
+         ;; (eval . (unless (member "docker-compose" python-shell-completion-native-disabled-interpreters)
+         ;;           (push "docker-compose" python-shell-completion-native-disabled-interpreters)))
+         ;; (counsel-gtags-gtags-extra-update-options-list . "--gtagslabel=pygments")
+         ))
+ (python-mode . ((comint-process-echoes . t))))
